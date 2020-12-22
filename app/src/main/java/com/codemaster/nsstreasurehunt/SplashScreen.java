ef7c0c11775a4258e73f05a82e86b09f213e6444
Subin <subinrajwwe@gmail.com>
2020-12-23 02:13:25 +0530

    First commit Splashscreen BG Done

--- app/src/main/java/com/codemaster/nsstreasurehunt/SplashScreen.java
+++ app/src/main/java/com/codemaster/nsstreasurehunt/SplashScreen.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -13,17 +15,25 @@ import android.widget.TextView;
 import com.codemaster.nsstreasurehunt.SharedPreference.SharedPreference;
 
 public class SplashScreen extends AppCompatActivity {
+
+
     ImageView logoSplashScreen;
-    TextView welcomeText;
-    Animation zoomAnimation, fadeAnimation;
+    Animation fadeAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+
 
         //initialization
         logoSplashScreen = findViewById(R.id.logo);
 
+        fadeAnimation = AnimationUtils.loadAnimation(SplashScreen.this, R.anim.fade);
 
 
 
