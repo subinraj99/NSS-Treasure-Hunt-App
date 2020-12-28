37b6fbf19ea3df9df61a420c7c755557f4e569ae
Subin <subinrajwwe@gmail.com>
2020-12-29 02:38:21 +0530

    Final

--- app/src/main/java/com/codemaster/nsstreasurehunt/DevScreen.java
+++ app/src/main/java/com/codemaster/nsstreasurehunt/DevScreen.java
@@ -1,27 +1,53 @@
 package com.codemaster.nsstreasurehunt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.codemaster.nsstreasurehunt.SharedPreference.SharedPreference;
 
 public class DevScreen extends AppCompatActivity {
 
-    TextView devby;
+    TextView devby,vishnu,fajar,aswin,subin,sou;
+    Animation fadeAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dev_screen);
-        devby=findViewById(R.id.devby);
+        devby = findViewById(R.id.devby);
+        vishnu = findViewById(R.id.vishnu);
+        fajar = findViewById(R.id.fajar);
+        sou = findViewById(R.id.sou);
+        aswin = findViewById(R.id.aswin);
+        subin = findViewById(R.id.subin);
 
-        devby.animate().translationYBy(350).setDuration(2000).setStartDelay(600);
-        devby.animate().alphaBy(1).setDuration(500).setStartDelay(600);
+        devby.animate().translationYBy(-250).setDuration(1000).setStartDelay(1500);
+        vishnu.animate().translationYBy(-250).setDuration(1000).setStartDelay(1500);
+        fajar.animate().translationYBy(-250).setDuration(1000).setStartDelay(1500);
+        aswin.animate().translationYBy(-250).setDuration(1000).setStartDelay(1500);
+        subin.animate().translationYBy(-250).setDuration(1000).setStartDelay(1500);
+        sou.animate().translationYBy(-250).setDuration(1000).setStartDelay(1500);
 
-        Intent DevScreenIntent = new Intent(DevScreen.this, LoginActivity.class);
-        startActivity(DevScreenIntent);
+        devby.animate().alphaBy(1).setDuration(1000).setStartDelay(1500);
+        vishnu.animate().alphaBy(1).setDuration(1000).setStartDelay(1500);
+        fajar.animate().alphaBy(1).setDuration(1000).setStartDelay(1500);
+        aswin.animate().alphaBy(1).setDuration(1000).setStartDelay(1500);
+        subin.animate().alphaBy(1).setDuration(1000).setStartDelay(1500);
+        sou.animate().alphaBy(1).setDuration(1000).setStartDelay(1500);
 
-        finish();
+        fadeAnimation = AnimationUtils.loadAnimation(DevScreen.this, R.anim.fade);
+
+        new Handler().postDelayed(() -> {
+            Intent mainIntent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(mainIntent);
+            finish();
+
+        },4500);
     }
 }
